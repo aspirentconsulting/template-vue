987235e32103d0011dec2abc30b1595f26b0936e
MichaelB <mjbradvica@protonmail.com>
2023-02-17 11:14:44 -0500

    updated tests and git ignore

--- src/app/App.spec.ts
+++ src/app/App.spec.ts
@@ -1,10 +1,31 @@
 import App from "./App.vue";
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 describe("App", () => {
   it("renders header correctly", () => {
     render(App);
 
     expect(screen.getByText(/acme sprocket co/i)).toBeInTheDocument();
+  });
+
+  it("counter increments correctly", async () => {
+    const user = userEvent.setup();
+
+    render(App);
+
+    await user.click(screen.getByRole("button", { name: /increment/i }));
+
+    expect(await screen.findByText(1)).toBeInTheDocument();
+  });
+
+  it("counter decrements correctly", async () => {
+    const user = userEvent.setup();
+
+    render(App);
+
+    await user.click(screen.getByRole("button", { name: /decrement/i }));
+
+    expect(await screen.findByText(-1)).toBeInTheDocument();
   });
 });
