e30823e6e43a5342139969f63378df02f7858891
MichaelB <mjbradvica@protonmail.com>
2023-02-17 11:09:56 -0500

    working unit tests

--- src/app/App.spec.ts
+++ src/app/App.spec.ts
@@ -1,5 +1,10 @@
-import { describe } from "node:test";
+import App from "./App.vue";
+import { render, screen } from "@testing-library/vue";
 
 describe("App", () => {
-  it("renders header correctly", () => {});
+  it("renders header correctly", () => {
+    render(App);
+
+    expect(screen.getByText(/acme sprocket co/i)).toBeInTheDocument();
+  });
 });
